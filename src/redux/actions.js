807c2c639cf54ac7db5927ad4be1849db42ce6e8
Bill <ceriustech@gmail.com>
2021-03-15 08:52:27 -0500

    Deleted the project to start a new one.

--- src/redux/actions.js
+++
@@ -1,9 +0,0 @@
-import { initialState } from "./root-reducer";
-
-export const addTodoItem = (inputvalue) => ({
-  type: "addTodos",
-  payload: {
-    todo: inputvalue,
-    id: initialState.todos.length + 1,
-  },
-});
--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-
-import ToDoReducer from "./root-reducer";
-
-const store = createStore(ToDoReducer);
-
-export default store;
